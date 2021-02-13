30493896b3fce75a1652f36942ae26bc233f4eb8
MegaStack <59016455+MegaStack@users.noreply.github.com>
2021-02-13 21:53:13 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package com.gmail.oastro36;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> intList = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			intList.add(getRandomInt());
+		}
+		System.out.println("List before removing: " + intList);
+		
+		intList.remove(0);
+		intList.remove(0);
+		intList.remove(intList.size() - 1);
+		System.out.println("List after removing: " + intList);
+
+	}
+
+	public static int getRandomInt() {
+		int gen = (int) (Math.random() * 100);
+		return gen;
+	}
+}
